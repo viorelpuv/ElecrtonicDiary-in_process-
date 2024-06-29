d188b0d7c56d8e6a4f937977fdf86e2b83227db6
Nikita <jjdjdjcjx@bk.ru>
2024-06-29 12:58:55 +0300

    Добавил бд MySQL

---
+++ database.py
@@ -0,0 +1,31 @@
+from mysql.connector import connect
+from bot.handlers.hashing import Encryptor
+
+
+class Database:
+    def __init__(self):
+        self.con = connect(
+            host='localhost',
+            user='root',
+            password=Encryptor().Decrypted('❄❆❆❄❆❆❆❆❄❄❆❆❆❄❄❆❄❆❄❄❆❄❄❆❄❄❆❆❄❆❆❆'
+                                           '❄❄❆❄❄❆❄❄❄❄❆❆❆❄❄❄❄❆❄❄❆❆❄❆❄❆❄❆❄❆❄❆')
+        )
+        self.cur = self.con.cursor()
+        self.cur.execute("""use data""")
+
+    def add_user(self, user_id, username):
+        self.cur.execute("""INSERT INTO telegram_users (user_id, username) VALUES (%s, %s)""",
+                         (user_id, username))
+        self.con.commit()
+
+    def select_users(self, user_id):
+        self.cur.execute("""SELECT * FROM telegram_users WHERE user_id = %s;""", (user_id,))
+        return self.cur.fetchall()
+
+    def select_usernames(self, login):
+        self.cur.execute("""SELECT * FROM telegram_users WHERE Логин = %s""", (login,))
+        return self.cur.fetchall()
+
+    def __del__(self):
+        self.cur.close()
+        self.con.close()
